75d050b4789c859a235b675279d03260daa43a7d
Ryo <kanadajinlee@yahoo.ca>
2017-05-28 18:30:43 -0500

    Got the home page started

--- src/App.js
+++ src/App.js
@@ -8,11 +8,15 @@ import {
   Scene
 } from 'react-native-router-flux';
 
+import {
+  Platform //ユーザーの使ってる機会を知るため
+} from 'react-native';
+
 export default class App extends Component {
   render(){
     return (
       <Router>
-        <Scene key="root">
+        <Scene key="root" style={{paddingTop: Platform.OS === 'ios' ? 65 : 55}}>
           <Scene key="home" component={Home} title="Home"/>
           <Scene key="chat" component={Chat} title="Chat"/>
         </Scene>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,57 @@
+'use strict';
 import React, { Component } from 'react';
 import {
   View,
-  Text
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
 } from 'react-native';
 
 export default class Home extends Component {
   render(){
+    const { title, nameInput, buttonText } = styles;
+
     return (
       <View>
-        <Text> Hello Home</Text>
+        <Text style={title}> 
+          Enter your name:
+        </Text>
+        <TextInput
+          style={nameInput}
+          placeholder="ex. Ronald Dump"
+        />
+        <TouchableOpacity
+          onPress={() => {
+            //navigate to the 2nd screen
+          }}
+        >
+          <Text style={buttonText}>
+            Next
+          </Text>
+        </TouchableOpacity>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  title: {
+    marginTop: 20,
+    marginLeft: 20,
+    fontSize: 20,
+    fontWeight: '600'
+  },
+  nameInput: {
+    height: 40,
+    borderWidth: 2,
+    margin: 20,
+    borderRadius: 7,
+    padding: 30
+  },
+  buttonText: {
+    marginLeft: 20,
+    fontSize: 20
+  }
+
+})
